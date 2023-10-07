11d1a7fdec6911170d8bec00e6aae6df1940d3d7
Deepakpal22 <115358288+Deepakpal22@users.noreply.github.com>
2023-10-07 16:49:33 +0000

    variables in js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 25565;
+let accountEmail = "deepak@google.com";
+var accountPassword = "12345";
+accountCity = "Delhi";
+
+
+accountEmail = "changedMail@gmail.com";
+accountPassword = "12345@"
+accountCity = "Bengalore"
+
+console.log(accountId)
+
+// Prefer not to use "var" instead use "let" to avoid block and functional scope problem in js
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
